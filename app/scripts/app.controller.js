9f3202ced2a23b94d428059f0199e24d2c7b7b60
Rish <rish@users.noreply.github.com>
2015-12-04 14:09:24 +0000

    Inject Courses Service into Application Controller

--- app/scripts/app.controller.js
+++ app/scripts/app.controller.js
@@ -2,7 +2,16 @@
   angular
     .module('app')
     .controller('AppController', AppController);
-  function AppController() {
+  function AppController(
+    $scope,
+    Courses
+  ) {
     console.info('AppController');
+
+    var vm = this;
+
+    Courses.get().then(function(response) {
+      console.info('Courses', response);
+    });
   }
 }());
